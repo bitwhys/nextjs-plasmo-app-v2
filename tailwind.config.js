a969b47fafee6a7d4a157c6a30a130fa5de7e356
George Bockari <bitwhys@gmail.com>
2023-05-15 17:36:16 -0400

    chore: add variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,17 @@
+const { mauve, mauveDark, plum, plumDark, red, redDark, mint, mintDark } = require('@radix-ui/colors')
+
+const radixToTailwind = radixColorObject => {
+  const captureScaleRegEx = new RegExp(/(\d{1,2})/)
+  const color = {}
+  for (const [token, value] of Object.entries(radixColorObject)) {
+    const match = token.match(captureScaleRegEx)
+    if (match) {
+      Object.assign(color, { [match[1]]: value })
+    }
+  }
+  return color
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,12 +21,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        canvas: 'hsl(var(--canvas))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--background))',
+        muted: 'hsl(var(--background))',
+        'muted-foreground': 'hsl(var(--background))',
+        accent: 'hsl(var(--background))',
+        'accent-foreground': 'hsl(var(--background))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
